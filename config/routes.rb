e40ab9deb4abf28b996921ca1df58de393150ee5
Thiago Silveira <thiago1992@yahoo.com>
2022-07-07 16:15:36 -0600

    Add gem and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/parks', to: 'parks#index' #accepting request and routing to parks controller with index action
 end
