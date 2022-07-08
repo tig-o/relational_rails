74d38e748f158071db3be237c4cb20b549cab6ad
Thiago Silveira <thiago1992@yahoo.com>
2022-07-07 18:09:50 -0600

    Add/Feat: Parks controller instance variable to access all saved parks

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,5 @@
 class ParksController < ApplicationController
     def index #after this, we need a view(file with same name as action). Not an empty whitescreen
+        @parks = Park.all
     end
 end
