90fc29902e1e803c7d77a9b28363ba1d9c690ece
Thiago Silveira <thiago1992@yahoo.com>
2022-07-08 23:57:54 -0600

    Add: Route for parks#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/parks', to: 'parks#index' #accepting request and routing to parks controller with index action
+  get '/parks/:id', to: 'parks#show' #id of parks sending request to parks controller and show action (to render specific html)
 end
