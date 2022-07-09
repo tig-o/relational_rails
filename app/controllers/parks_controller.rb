44f4046351b29157227faac2c2e8db7864df8be6
Thiago Silveira <thiago1992@yahoo.com>
2022-07-09 15:58:41 -0600

    Feature: Parks controller show method finds park by id

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,4 +2,9 @@ class ParksController < ApplicationController
     def index #after this, we need a view(file with same name as action). Not an empty whitescreen
         @parks = Park.all
     end
+
+    def show
+        # binding.pry
+        @park = Park.find(params[:id])
+    end
 end
