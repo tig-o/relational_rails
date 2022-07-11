b5854f09a0a4489c61e3c397676b0b89426f8cc6
Thiago Silveira <thiago1992@yahoo.com>
2022-07-11 00:25:52 -0600

    Update: Show spec test to display trail and attributes according to user-input id for trails

--- spec/features/trails/show_spec.rb
+++ spec/features/trails/show_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe 'the trails show page' do
     # When I visit '/child_table_name/:id'
     # Then I see the child with that id including the child's attributes:
     it 'displays trail and attributes according to user-input id' do
-        trail = Trail.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
-        trail = Trail.create!(name: "Sky Pond via Glacier Gorge Trail", difficulty: "hard", length: 9.4, elevation_gain: 1758, dogs_allowed: false)
         trail = Trail.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
 
         visit "/trails/#{trail.id}"
+
+        # save_and_open_page
+
+        expect(page).to have_content(trail.id)
+        expect(page).to have_content(trail.name)
+        expect(page).to have_content(trail.difficulty)
+        expect(page).to have_content(trail.length)
+        expect(page).to have_content(trail.elevation_gain)
+        expect(page).to have_content(trail.dogs_allowed)
     end
 end
