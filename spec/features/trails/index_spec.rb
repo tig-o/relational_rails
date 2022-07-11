6ff3c799b99ee1d41e7d0fd3daacc3f807682cfb
Thiago Silveira <thiago1992@yahoo.com>
2022-07-10 23:09:25 -0600

    Update: Trails index spec

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -8,8 +8,17 @@ RSpec.describe 'the trails index page' do
     # Then I see each Child in the system including the Child's attributes:
     it 'displays the trails name and attributes' do
         trails = Trail.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
-        visit "/trails"
+        trails = Trail.create!(name: "Sky Pond via Glacier Gorge Trail", difficulty: "hard", length: 9.4, elevation_gain: 1758, dogs_allowed: false)
+        trails = Trail.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
 
+        visit "/trails"
         
+        save_and_open_page
+
+        expect(page).to have_content(trails.name)
+        expect(page).to have_content(trails.difficulty)
+        expect(page).to have_content(trails.length)
+        expect(page).to have_content(trails.elevation_gain)
+        expect(page).to have_content(trails.dogs_allowed)
     end
 end
