ff794f35007a89e32dacfebb48c1f588e797cc72
Thiago Silveira <thiago1992@yahoo.com>
2022-07-10 22:53:46 -0600

    Update: Trails index test with trails create

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe 'the trails index page' do
     # When I visit '/child_table_name'
     # Then I see each Child in the system including the Child's attributes:
     it 'displays the trails name and attributes' do
+        trails = Trail.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
         visit "/trails"
+
+        
     end
 end
