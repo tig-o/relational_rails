7cbcc3e2083c4a4feab9bebc642d418133796efa
Thiago Silveira <thiago1992@yahoo.com>
2022-07-10 23:38:38 -0600

    Update: Index method in trails controller

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,4 +1,5 @@
 class TrailsController < ApplicationController #view directories map to name of controller
     def index #view filenames map to name of actions/methods
+        @trails = Trail.all
     end
 end
