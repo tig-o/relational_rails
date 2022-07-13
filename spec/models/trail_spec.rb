4fd62bc971ea407a815aab2d739189cbf36dd8a9
Thiago Silveira <thiago1992@yahoo.com>
2022-07-13 11:01:21 -0600

    Refactor: Test model validations and associations, now passing

--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Trail, type: :model  do
-    it { should belong_to :park }
+RSpec.describe Trail, type: :model do
+    describe 'relationships' do
+        it { should belong_to :park }
+    end
+
+    describe 'validations' do # when i create a park and it has a name, it should have a name
+        it { should validate_presence_of :name }
+        it { should validate_presence_of :difficulty }
+        it { should validate_presence_of :length }
+        it { should validate_presence_of :elevation_gain }
+        it { should allow_value(true).for :dogs_allowed}
+        it { should allow_value(false).for :dogs_allowed}
+        it { should_not allow_value(nil).for :dogs_allowed}
+    end
 end
