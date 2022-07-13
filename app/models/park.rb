dfdfb25454d65505958f479928ab7f284190e1a6
Thiago Silveira <thiago1992@yahoo.com>
2022-07-13 10:58:17 -0600

    Refactor: Model validations for park and trail

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,10 @@
 class Park < ApplicationRecord
+    has_many :trails
+    
+    validates_presence_of :name
+    validates_presence_of :state
+    validates_presence_of :city
+    validates_presence_of :description
+    validates_presence_of :fee
+    validates_inclusion_of :open, :in => [true,false]
 end
