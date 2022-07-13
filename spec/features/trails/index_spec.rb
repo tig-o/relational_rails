f4358e7cad6d3382d2cea2d61a1b51ff6ecf05b5
Thiago Silveira <thiago1992@yahoo.com>
2022-07-13 11:05:13 -0600

    Refactor: Trails index spec to pass error where trail cannot be created without a park

--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -7,18 +7,31 @@ RSpec.describe 'the trails index page' do
     # When I visit '/child_table_name'
     # Then I see each Child in the system including the Child's attributes:
     it 'displays the trails name and attributes' do
-        trail = Trail.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
-        trail = Trail.create!(name: "Sky Pond via Glacier Gorge Trail", difficulty: "hard", length: 9.4, elevation_gain: 1758, dogs_allowed: false)
-        trail = Trail.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
+        rocky_mountain = Park.create!(name: "Rocky Mountain National Park", state: "CO", city: "Estes Park", description: "Rocky Mountain National Park in northern Colorado spans the Continental Divide and encompasses protected mountains, forests and alpine tundra. It's known for the Trail Ridge Road and the Old Fall River Road, drives that pass aspen trees and rivers.", fee: 35, open: true)
+        emerald = rocky_mountain.trails.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
+        skypond = rocky_mountain.trails.create!(name: "Sky Pond via Glacier Gorge Trail", difficulty: "hard", length: 9.4, elevation_gain: 1758, dogs_allowed: false)
+        ute = rocky_mountain.trails.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
 
         visit "/trails/"
 
         # save_and_open_page
 
-        expect(page).to have_content(trail.name)
-        expect(page).to have_content(trail.difficulty)
-        expect(page).to have_content(trail.length)
-        expect(page).to have_content(trail.elevation_gain)
-        expect(page).to have_content(trail.dogs_allowed)
+        expect(page).to have_content(emerald.name)
+        expect(page).to have_content(emerald.difficulty)
+        expect(page).to have_content(emerald.length)
+        expect(page).to have_content(emerald.elevation_gain)
+        expect(page).to have_content(emerald.dogs_allowed)
+
+        expect(page).to have_content(skypond.name)
+        expect(page).to have_content(skypond.difficulty)
+        expect(page).to have_content(skypond.length)
+        expect(page).to have_content(skypond.elevation_gain)
+        expect(page).to have_content(skypond.dogs_allowed)
+
+        expect(page).to have_content(ute.name)
+        expect(page).to have_content(ute.difficulty)
+        expect(page).to have_content(ute.length)
+        expect(page).to have_content(ute.elevation_gain)
+        expect(page).to have_content(ute.dogs_allowed)
     end
 end
--- spec/features/trails/show_spec.rb
+++ spec/features/trails/show_spec.rb
@@ -7,17 +7,19 @@ RSpec.describe 'the trails show page' do
     # When I visit '/child_table_name/:id'
     # Then I see the child with that id including the child's attributes:
     it 'displays trail and attributes according to user-input id' do
-        trail = Trail.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
+        rocky_mountain = Park.create!(name: "Rocky Mountain National Park", state: "CO", city: "Estes Park", description: "Rocky Mountain National Park in northern Colorado spans the Continental Divide and encompasses protected mountains, forests and alpine tundra. It's known for the Trail Ridge Road and the Old Fall River Road, drives that pass aspen trees and rivers.", fee: 35, open: true)
+        emerald = rocky_mountain.trails.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
+        skypond = rocky_mountain.trails.create!(name: "Sky Pond via Glacier Gorge Trail", difficulty: "hard", length: 9.4, elevation_gain: 1758, dogs_allowed: false)
+        ute = rocky_mountain.trails.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
 
-        visit "/trails/#{trail.id}"
+        visit "/trails/#{emerald.id}"
 
         # save_and_open_page
 
-        expect(page).to have_content(trail.id)
-        expect(page).to have_content(trail.name)
-        expect(page).to have_content(trail.difficulty)
-        expect(page).to have_content(trail.length)
-        expect(page).to have_content(trail.elevation_gain)
-        expect(page).to have_content(trail.dogs_allowed)
+        expect(page).to have_content(emerald.name)
+        expect(page).to have_content(emerald.difficulty)
+        expect(page).to have_content(emerald.length)
+        expect(page).to have_content(emerald.elevation_gain)
+        expect(page).to have_content(emerald.dogs_allowed)
     end
 end
