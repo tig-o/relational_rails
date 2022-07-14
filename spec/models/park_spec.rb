d4e52de963d0c49425cea69ceb16e8252a9670b6
Thiago Silveira <thiago1992@yahoo.com>
2022-07-14 10:43:19 -0600

    Test: Park model displays trails amount per park

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -23,4 +23,15 @@ RSpec.describe Park, type: :model do
             expect(Park.order(created_at: :desc)).to eq([rocky_mountain, yellowstone])
         end
     end
+
+    describe 'trails_count' do
+        it 'displays trails amount' do
+            rocky_mountain = Park.create!(name: "Rocky Mountain National Park", state: "CO", city: "Estes Park", description: "Rocky Mountain National Park in northern Colorado spans the Continental Divide and encompasses protected mountains, forests and alpine tundra. It's known for the Trail Ridge Road and the Old Fall River Road, drives that pass aspen trees and rivers.", fee: 35, open: true)
+            emerald = rocky_mountain.trails.create!(name: "Emerald Lake Trail", difficulty: "moderate", length: 3.2, elevation_gain: 698, dogs_allowed: false)
+            skypond = rocky_mountain.trails.create!(name: "Sky Pond via Glacier Gorge Trail", difficulty: "hard", length: 9.4, elevation_gain: 1758, dogs_allowed: false)
+            ute = rocky_mountain.trails.create!(name: "Ute Trail: Alpine Visitor Center to Forest Canyon Pass", difficulty: "moderate", length: 3.5, elevation_gain: 364, dogs_allowed: true)
+
+            expect(rocky_mountain.trails_count).to eq(3)
+        end
+    end
 end
